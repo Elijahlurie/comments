93013af075265021cf0ca75bab55b2977686b95a
Elijahlurie <63481623+Elijahlurie@users.noreply.github.com>
2020-05-30 14:46:52 -0700

    Add files via upload

---
+++ comment_test/trying.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>comment system</title>
+  </head>
+  <body>
+    <ul>
+    <li>Username: <input type = "text" name = "username"></li>
+    <li>Comment: <input type = "text" name = "comment"></li>
+    </ul>
+
+  </body>
+</html>
